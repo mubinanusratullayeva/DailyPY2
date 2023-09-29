1850c5db91417af3925dba26be7efb22a0b238b6
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-09-29 05:44:34 -0700

    OOP some examples

--- lesson__first.py
+++ lesson__first.py
@@ -1,5 +1,55 @@
-f = open('data.txt', 'r')
+# Python File Handling
 
-res = [i for i in f]
-print(res)
+# f = open('data.txt', 'r')
+#
+# res = [i for i in f]
+# print(res)
 
+
+
+# OOP
+
+# class Shakl:
+#     a = 9
+#     b = 8
+#
+#     def multiple(self):
+#         return  self.a * self.b
+#
+#     def mult(self, a, b):
+#         return a * b
+#
+# shape = Shakl()
+
+
+
+# class Author:
+#     def __init__(self, name, last_name, age):
+#         self.name = name
+#         self.last_name = last_name
+#         self.age = age
+#
+# class Book:
+#     def __init__(self, name, author, price):
+#         self.name = name
+#         self.author = author
+#         self.price = price
+#
+# abdulla_qodiriy = Author('Abdulla', 'Qodiriy', 56)
+# utgan_kunlar = Book('O\'tgan Kunlar', abdulla_qodiriy, 4000000)
+#
+# print(utgan_kunlar.author.name, utgan_kunlar.author.last_name)
+
+
+
+
+# class Dog:
+#     species = "Canis familiaris"
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# miles = Dog("Miles", 4)
+# buddy = Dog("Buddy", 9)
+#
+# print(miles.name, miles.age)
